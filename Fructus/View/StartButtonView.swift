ba20bd0b69220e287907d1ea69b23d8f8e238bfc
Rodrigo Conte <rodrigo.conte14@gmail.com>
2021-09-09 17:07:31 -0400

    Feat: Include fruit card and a mocked tabbar to page from many cards with swipe

---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,45 @@
+//
+//  StartButtonView.swift
+//  StartButtonView
+//
+//  Created by Rodrigo Conte on 09/09/21.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    var body: some View {
+        Button {
+            print("Clicked")
+        } label: {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            } //: Hstack
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule().strokeBorder(Color.white, lineWidth: 1.25)
+            )
+        } //: Button
+        .accentColor(Color.white)
+        
+    }
+}
+
+
+// MARK: - Preview
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            StartButtonView()
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
