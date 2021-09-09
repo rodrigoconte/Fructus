c4b5cfcfa6fa0937e7c9c523078ace33b5890aba
Rodrigo Conte <rodrigo.conte14@gmail.com>
2021-09-09 16:04:39 -0400

    Init: Initial commit setting up the main resources to project

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rodrigo Conte on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
