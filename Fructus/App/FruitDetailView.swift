b668551425720b123e961dfa13974e406a671744
Rodrigo Conte <rodrigo.conte14@gmail.com>
2021-09-22 15:32:20 -0400

    feat: Created a fruits detail on app and included custom cell press action

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -14,7 +14,42 @@ struct FruitDetailView: View {
     
     // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    // Header
+                    FruitHeaderView(fruit: fruit)
+                    VStack(alignment: .leading, spacing: 20) {
+                        // Title
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // Headline
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        // Nutrients
+                        
+                        // Subheadline
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // Description
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        // Link
+                    } //: Vstack
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                } //: Vstack
+                .navigationBarHidden(true)
+            } //: Scroll
+            .edgesIgnoringSafeArea(.top)
+        } //: Navigation view
     }
 }
 
